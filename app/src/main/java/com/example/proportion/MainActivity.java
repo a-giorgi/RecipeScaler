7ed2c3cc6de6525aa4f52bbfdb99cbef9c56615f
Andrea Giorgi <tvcamera92@hotmail.it>
2024-04-23 23:17:31 +0200

    Added methods to open Camera

--- app/src/main/java/com/example/proportion/MainActivity.java
+++ app/src/main/java/com/example/proportion/MainActivity.java
@@ -1,22 +1,27 @@
 package com.example.proportion;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import com.example.proportion.databinding.ActivityMainBinding;
 import com.google.android.material.slider.Slider;
+
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.proportion.databinding.ActivityMainBinding;
+import androidx.activity.result.ActivityResultLauncher;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,11 +29,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -36,14 +38,16 @@ public class MainActivity extends AppCompatActivity implements AddElementDialog.
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
+    private ActivityResultLauncher<Intent> cameraLauncher;
     private LayoutInflater inflater;
-
     private RecyclerView listContentView;
     private ArrayList<Element> listContentElements;
     private RecyclerView.Adapter<ListElementViewHolder> listContentAdapter;
 
     private double currentScaleFactor = 1.0;
+
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
+    private static final String[] PERMISSIONS_REQUIRED = new String[]{android.Manifest.permission.CAMERA};
 
     private void showElementDialog(View view){
         AddElementDialog addElementDialog = new AddElementDialog(this);
@@ -146,14 +150,32 @@ public class MainActivity extends AppCompatActivity implements AddElementDialog.
         });
 
 
+        cameraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                Intent data = result.getData();
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                //To show it:
+                //imageView.setImageBitmap(photo);
+                //imageView.setVisibility(ImageView.VISIBLE);
+
+                // Call the OCR function
+
+            }
+        });
+
+    }
 
 
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraLauncher.launch(cameraIntent);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -161,7 +183,12 @@ public class MainActivity extends AppCompatActivity implements AddElementDialog.
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_scan) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS_REQUIRED, CAMERA_PERMISSION_REQUEST_CODE);
+            } else {
+                openCamera();
+            }
             return true;
         }
 
